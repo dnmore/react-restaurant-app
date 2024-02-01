1664ade8371998ad6c43d13cb98c56b4174653b2
Diana Moretti <morettidn@gmail.com>
2024-02-01 11:55:36 +0100

    setting up navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Menu = () => {
+  return (
+    <div>
+      <h1>Menu</h1>
+    </div>
+  );
+};
+
+const SignIn = ()=> {
+  return(
+    <div>
+      <h1>Sign In</h1>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="menu" element={<Menu />} />
+        <Route path="signin" element={<SignIn/> }/>
+      </Route>
     </Routes>
   );
 };
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation-container">
+        <Link className="logo-container" to={"/"}>
+          <div className="logo"><strong>Crave</strong>Waves</div>
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to={"/menu"}>
+            Menu
+          </Link>
+          <Link className="nav-link" to={"/signin"}>
+            Sign In
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
