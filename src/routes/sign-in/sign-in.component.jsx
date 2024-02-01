f5dad800241a89d1a67e8c4966c38c239b6edb56
Diana Moretti <morettidn@gmail.com>
2024-02-01 18:30:24 +0100

    adding sign-in component and sign-in with google logic

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,29 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
-  signInWithGooglePopup,
+  auth,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef =await createUserDocumentFromAuth(user);
-  };
+  useEffect(() => {
+    async function fetchData() {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+
+      <button onClick={signInWithGoogleRedirect}>Sign in with Google</button>
     </div>
   );
 };
