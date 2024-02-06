3341eabdbe9083243b9645df9f20f8a797c967ec
Diana Moretti <morettidn@gmail.com>
2024-02-06 08:52:11 +0100

    adding cart icon, dropdown and context

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,16 @@
+import './cart-dropdown.styles.scss'
+
+const CartDropdown = ()=> {
+    return(
+        <div className='cart-dropdown-container'>
+            <div className='cart-items' />
+            <button>CHECKOUT</button>
+
+        </div>
+    )
+}
+
+
+
+
+export default CartDropdown;
