078a355b15c5c673cbe15d5edef88c6601fcd90b
Diana Moretti <morettidn@gmail.com>
2024-02-14 14:39:23 +0100

    updating checkout styles

--- src/components/checkout-card/checkout-card.styles.jsx
+++ src/components/checkout-card/checkout-card.styles.jsx
@@ -1,39 +1,40 @@
 import styled from "styled-components";
 
-export const CheckoutCardContainer = styled.div `
-width: 200px;
-display: flex;
-flex-direction: column;
-border: 1px solid black;
-`
+export const CheckoutCardContainer = styled.div`
+  width: 200px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+`;
 
-export const CheckoutCardImage = styled.div `
-height: 200px;
-overflow: hidden;
+export const CheckoutCardImage = styled.div`
+  height: 200px;
+  overflow: hidden;
 
-    img {
-      height: 100%;
-      width: 100%;
-      object-fit: cover;
-    }
-`
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+`;
 
-export const CheckoutCardBody = styled.div `
-display: flex;
-flex-direction: column;
-align-items: center;
-`
+export const CheckoutCardBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-export const Quantity = styled.div `
-display: flex;
-align-items: center;
-gap: 20px;
-`
+export const Quantity = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
 
-export const QuantityButton = styled.button `
-border: none;
-background-color: transparent;
-font: inherit;
-font-weight: bold;
-cursor: pointer;
-`
+export const QuantityButton = styled.button`
+  border: none;
+  background-color: transparent;
+  font: inherit;
+  font-weight: bold;
+  cursor: pointer;
+`;
--- src/routes/checkout/checkout.styles.jsx
+++ src/routes/checkout/checkout.styles.jsx
@@ -1,22 +1,18 @@
 import styled from "styled-components";
 
-export const CheckoutContainer = styled.div `
+export const CheckoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+`;
+export const CheckoutBody = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  margin-bottom: 30px;
+`;
 
-display: flex;
-flex-direction: column;
-place-items: center;
-
-`
-export const CheckoutBody = styled.div `
-display: flex;
-    flex-wrap: wrap;
-    gap: 20px;
-    margin-bottom: 30px;
-`
-
-export const Total = styled.span `
-font-weight: bold;
-font-size: 20px;
-`
-
-
+export const Total = styled.span`
+  font-weight: bold;
+  font-size: 20px;
+`;
